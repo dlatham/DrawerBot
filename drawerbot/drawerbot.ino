0d18f09ed9bb226fd547ee22595916858fb9148e
Dave Latham <dave.latham@gmail.com>
2018-03-10 14:30:47 -0800

    Lots of bug fixes

--- drawerbot/drawerbot.ino
+++ drawerbot/drawerbot.ino
@@ -12,14 +12,14 @@
 
 //PIN DEFINITIONS
 #define requestPin 2          //Pin to monitor for requests - interrupt
-#define motorDirA 8
-#define motorDirB 9
-#define drawerRelay 10
-#define liftRelay 11
+#define motorDirA A0
+#define motorDirB A1
+#define drawerRelay A2
+#define liftRelay A3
 #define ledData 12
-#define drawerLimitIn A0
-#define drawerLimitOut A1
-#define liftLimit A2
+#define drawerLimitIn 8
+#define drawerLimitOut 9
+#define liftLimit 10
 //#define CLK 13       // SPI Clock, shared with SD card
 //#define MISO 12      // Input data, from VS1053/SD card
 //#define MOSI 11      // Output data, to VS1053/SD card
@@ -39,6 +39,11 @@
 //MOTION DIRECTION CONFIGURATION
 #define drawerMotorA LOW      //Set the output of direction relay A for the drawer in forward
 #define liftMotorA HIGH       //Set the output of direction relay A for the lift in down
+#define drawer 0
+#define lift 1
+#define forward 2
+#define reverse 3
+unsigned long current;
 
 
  
@@ -54,6 +59,7 @@ void setup() {
   pinMode(liftRelay, OUTPUT);
   digitalWrite(liftRelay, HIGH);
   pinMode(drawerLimitIn, INPUT_PULLUP);
+  pinMode(drawerLimitOut, INPUT_PULLUP);
   pinMode(liftLimit, INPUT_PULLUP);
 
   //ADAFRUIT HARDWARE SETUP
@@ -75,10 +81,12 @@ void loop() {
     switch(incomingByte){
       case 111: {
         if(drawerOut()){liftDown();}
+        printStatus();
       }
       break;
       case 99: {
         if(liftUp()){drawerIn();}
+        printStatus();
       }
       break;
     }
@@ -91,31 +99,31 @@ void loop() {
 bool drawerOut(){                                                           //Drawer out
   //Check status
   if(isDrawerOut()){
-    Serial.print("DRAWER: Drawer already out, returning true.");
+    Serial.println(F("DRAWER: Drawer already out, returning true."));
     return true;
   }
   //Set motion
-  if(!setDirection("drawer", "forward")){
-    Serial.println("DRAWER: Error, set direction failed.");
+  if(!setDirection(drawer, forward)){
+    Serial.println(F("DRAWER: Error, set direction failed."));
     return false;
   }
   //Confirm the lift is up
   if(!isLiftUp()){
-    Serial.print("DRAWER: Motion canceled because the lift isn't up.");
+    Serial.println(F("DRAWER: Motion canceled because the lift isn't up."));
     return false;
   }
-  Serial.print("DRAWER: Opening... ");
-  unsigned long current = millis();
-  while(drawerLimitOut == HIGH){
+  Serial.print(F("DRAWER: Opening... "));
+  current = millis();
+  while(digitalRead(drawerLimitOut) == HIGH){
     digitalWrite(drawerRelay, LOW);
     if((millis() - current) >= drawerTimeout){
       digitalWrite(drawerRelay, HIGH);
-      Serial.print("Failed. Drawer timed out.");
+      Serial.print(F("Failed. Drawer timed out."));
       return false;
     }
   }
   digitalWrite(drawerRelay, HIGH);
-  Serial.print("OK (Completed in ");
+  Serial.print(F("OK (Completed in "));
   Serial.print(millis()-current);
   Serial.println("ms)");
   return true;
@@ -124,21 +132,21 @@ bool drawerOut(){                                                           //Dr
 bool drawerIn(){                                                            //Drawer in
   //Check to see if the drawer is already in
   if(isDrawerIn()){
-    Serial.print("DRAWER: Drawer already out, returning true.");
+    Serial.println(F("DRAWER: Drawer already out, returning true."));
     return true;
   }
   if(!isLiftUp()){
-    Serial.print("DRAWER: Motion canceled because the lift isn't up.");
+    Serial.println(F("DRAWER: Motion canceled because the lift isn't up."));
     return false;
   }
   //Set motion
-  if(!setDirection("drawer", "reverse")){
-    Serial.println("DRAWER: Error, set direction failed.");
+  if(!setDirection(drawer, reverse)){
+    Serial.println(F("DRAWER: Error, set direction failed."));
     return false;
   }
-  Serial.print("DRAWER: Closing... ");
-  unsigned long current = millis();
-  while(drawerLimitIn == HIGH){
+  Serial.print(F("DRAWER: Closing... "));
+  current = millis();
+  while(digitalRead(drawerLimitIn) == HIGH){
     digitalWrite(drawerRelay, LOW);
     if((millis() - current) >= drawerTimeout){
       digitalWrite(drawerRelay, HIGH);
@@ -165,12 +173,12 @@ bool liftDown(){                                                                
     return false;
   }
   //Set motion
-  if(!setDirection("lift", "forward")){
+  if(!setDirection(lift, forward)){
     Serial.println("LIFT: Error, set direction failed.");
     return false;
   }
   Serial.print("LIFT: Lowering... ");
-  unsigned long current = millis();
+  current = millis();
   while((millis()-current) < lowerTime){
     digitalWrite(liftRelay, LOW);
   }
@@ -183,7 +191,7 @@ bool liftDown(){                                                                
 
 bool liftUp(){                                                                        //Lift Up
   //Check to see if the lift is up already
-  if(!isLiftUp()){
+  if(isLiftUp()){
     Serial.println("LIFT: Motion canceled because the lift is already up.");
     return false;
   }
@@ -193,13 +201,13 @@ bool liftUp(){                                                                  
     return false;
   }
   //Set motion
-  if(!setDirection("lift", "reverse")){
+  if(!setDirection(lift, reverse)){
     Serial.println("LIFT: Error, set direction failed.");
     return false;
   }
   Serial.print("LIFT: Raising... ");
-  unsigned long current = millis();
-  while(liftLimit == HIGH){
+  current = millis();
+  while(digitalRead(liftLimit) == HIGH){
     digitalWrite(liftRelay, LOW);
     if((millis() - current) >= (lowerTime + 1000)){
       digitalWrite(liftRelay, HIGH);
@@ -214,9 +222,9 @@ bool liftUp(){                                                                  
   return true;
 }
 
-bool setDirection(char type, char dir){
+bool setDirection(uint8_t type, uint8_t dir){
   Serial.print("SETDIRECTION: ");
-  if(type == "drawer" && dir == "forward"){           //DRAWER FORWARD
+  if(type == 0 && dir == 2){           //DRAWER FORWARD
     digitalWrite(motorDirA, drawerMotorA);
     digitalWrite(motorDirB, !drawerMotorA);
     Serial.print("Drawer : Relay A ");
@@ -224,7 +232,7 @@ bool setDirection(char type, char dir){
     Serial.print(", Relay B ");
     Serial.println(!drawerMotorA);
     return true;
-  } else if(type == "drawer" && dir == "reverse"){    //DRAWER REVERSE
+  } else if(type == 0 && dir == 3){    //DRAWER REVERSE
     digitalWrite(motorDirA, !drawerMotorA);
     digitalWrite(motorDirB, drawerMotorA);
     Serial.print("Drawer : Relay A ");
@@ -232,7 +240,7 @@ bool setDirection(char type, char dir){
     Serial.print(", Relay B ");
     Serial.println(drawerMotorA);
     return true;
-  } else if(type == "lift" && dir == "forward"){      //LIFT FORWARD
+  } else if(type == 1 && dir == 2){      //LIFT FORWARD
     digitalWrite(motorDirA, liftMotorA);
     digitalWrite(motorDirB, !liftMotorA);
     Serial.print("Lift : Relay A ");
@@ -240,7 +248,7 @@ bool setDirection(char type, char dir){
     Serial.print(", Relay B ");
     Serial.println(!liftMotorA);
     return true;
-  } else if(type == "lift" && dir == "reverse"){      //LIFT REVERSE
+  } else if(type == 1 && dir == 3){      //LIFT REVERSE
     digitalWrite(motorDirA, !liftMotorA);
     digitalWrite(motorDirB, liftMotorA);
     Serial.print("Lift : Relay A ");
@@ -256,7 +264,7 @@ bool setDirection(char type, char dir){
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! STATUS FUNCTIONS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 bool isDrawerIn(){
-  if(drawerLimitIn == LOW){
+  if(digitalRead(drawerLimitIn) == 0){
     Serial.println("DRAWER: Status IN : TRUE");
     return true;
   } else {
@@ -266,7 +274,7 @@ bool isDrawerIn(){
 }
 
 bool isDrawerOut(){
-  if(drawerLimitOut == LOW){
+  if(digitalRead(drawerLimitOut) == LOW){
     Serial.println("DRAWER: Status OUT : TRUE");
     return true;
   } else {
@@ -276,7 +284,7 @@ bool isDrawerOut(){
 }
 
 bool isLiftUp(){
-  if(liftLimit == LOW){
+  if(digitalRead(liftLimit) == LOW){
     Serial.println("LIFT: Status UP : TRUE");
     return true;
   } else {
@@ -286,28 +294,43 @@ bool isLiftUp(){
 }
 
 void printStatus(){
-  Serial.println("\r\r--------------------------");
-  Serial.println("STATUS:\rRequest\tMotorDirA\tMotorDirB\tDrawer\tLift");
+  Serial.println("\n\n--------------------------");
+  Serial.println("STATUS:\nRequest\tMotorDirA\tMotorDirB\tDrawer\tLift");
   Serial.print(requestPin, digitalRead(requestPin));
   Serial.print("\t");
-  Serial.print(motorDirA, digitalRead(motorDirA));
+  Serial.print(motorDirA);
+  Serial.print("-");
+  Serial.print(digitalRead(motorDirA));
+  Serial.print("\t\t");
+  Serial.print(motorDirB);
+  Serial.print("-");
+  Serial.print(digitalRead(motorDirB));
+  Serial.print("\t\t");
+  Serial.print(drawerRelay);
+  Serial.print("-");
+  Serial.print(digitalRead(drawerRelay));
   Serial.print("\t");
-  Serial.print(motorDirB, digitalRead(motorDirB));
-  Serial.print("\t");
-  Serial.print(drawerRelay, digitalRead(drawerRelay));
-  Serial.print("\t");
-  Serial.print(liftRelay, digitalRead(liftRelay));
-  Serial.println("\r\rLimit In\tLimit Out\tLimit Up\tDrawer Timeout\tLift Time");
-  Serial.print(drawerLimitIn, digitalRead(drawerLimitIn));
-  Serial.print("\t");
-  Serial.print(drawerLimitOut, digitalRead(drawerLimitOut));
-  Serial.print("\t");
-  Serial.print(liftLimit, digitalRead(liftLimit));
-  Serial.print("\t");
+  Serial.print(liftRelay);
+  Serial.print("-");
+  Serial.print(digitalRead(liftRelay));
+  Serial.println("\n\nLimit In\tLimit Out\tLimit Up\tDrawer Timeout\tLift Time");
+  Serial.print(drawerLimitIn);
+  Serial.print("-");
+  Serial.print(digitalRead(drawerLimitIn));
+  Serial.print("\t\t");
+  Serial.print(drawerLimitOut);
+  Serial.print("-");
+  Serial.print(digitalRead(drawerLimitOut));
+  Serial.print("\t\t");
+  Serial.print(liftLimit);
+  Serial.print("-");
+  Serial.print(digitalRead(liftLimit));
+  Serial.print("\t\t");
   Serial.print(drawerTimeout/1000);
-  Serial.print("sec\t");
-  Serial.println(lowerTime/1000);
-  Serial.println("---------------------------------\r[O]pen, [C]lose, [D]rawer, [L]ift\rREADY.");
+  Serial.print("sec\t\t");
+  Serial.print(lowerTime/1000);
+  Serial.println("sec");
+  Serial.println("---------------------------------\n[O]pen, [C]lose, [D]rawer, [L]ift\nREADY.");
 }
 
 
